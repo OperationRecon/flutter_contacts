8dee10163b3f92c696ee153f5d9a8c0ef58d7f4d
OperationRecon <mohammadnaas203@gmail.com>
2023-09-22 17:59:50 +0200

    adding the widget file, which for somereason didn't get added with merge.

---
+++ code/lib/widgets/contact_list_entry.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_contacts/flutter_contacts.dart';
+import 'contact_overlay.dart';
+
+// Contains the widget used to Show the contact in lists, such as the homepage and the search results page.
+
+class ContactListEntry extends StatefulWidget {
+  const ContactListEntry({
+    super.key,
+    required this.contactData,
+  });
+
+  final Contact contactData;
+
+  @override
+  State<StatefulWidget> createState() => _ContactListEntryState();
+  
+}
+
+class _ContactListEntryState extends State<ContactListEntry> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+              shadowColor: Theme.of(context).colorScheme.onBackground,
+              child: Container(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 8.0,
+                ),
+                color: Theme.of(context).primaryColor,
+                child: TextButton.icon(
+                  icon: widget.contactData.thumbnail != null
+                    // creates the circular avatar besides the entry's name, if the contact has a thumbnail
+                    // it uses that. Otherwise it creates an avatar using the firstmost letter in the contact
+                    // and a color unique to it.
+                      ? CircleAvatar(
+                          foregroundImage:
+                              MemoryImage(widget.contactData.thumbnail!))
+                      : CircleAvatar(
+                          backgroundColor:
+                              Color(widget.contactData.hashCode).withAlpha(80),
+                          child: Text(widget.contactData.displayName[0]),
+                        ),
+                  // creates on overlay that has the contact's detilas within
+                  onPressed: () => createContactOverlay(
+                      context,
+                      FlutterContacts.getContact(widget.contactData.id,
+                          withProperties: true, withThumbnail: true)),
+                  style: const ButtonStyle(
+                    alignment: AlignmentDirectional.centerStart,
+                    shape: MaterialStatePropertyAll(BeveledRectangleBorder()),
+                  ),
+                  label: Text(
+                    widget.contactData.displayName,
+                    style: Theme.of(context).textTheme.headlineSmall,
+                  ),
+                ),
+              ),
+            );
+  }
+
+}
