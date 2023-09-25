4cddd5dd87ae6f27ef9855d777c466bc1170a5bd
CollCaz <62307305+CollCaz@users.noreply.github.com>
2023-09-25 13:35:05 +0200

    Added if statement to display an empty page while loading.

--- code/lib/widgets/contact_overlay.dart
+++ code/lib/widgets/contact_overlay.dart
@@ -1,3 +1,4 @@
+import 'package:code/widgets/main_app_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_contacts/flutter_contacts.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -42,7 +43,7 @@ class ContactOverlay extends StatefulWidget {
 class _ContactOverlayState extends State<ContactOverlay> {
   bool editing = false;
   Contact? contactData = Contact(
-    id: "1",
+    id: "Loading",
     displayName: "Loading",
   );
 
@@ -52,89 +53,95 @@ class _ContactOverlayState extends State<ContactOverlay> {
     _loadData();
 
     // create the overlay containing the contactEntry
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text("Details"),
-        leading: BackButton(
-          onPressed: () => removeHighlightOverlay(),
-        ),
-        actions: [
-          IconButton(
-            onPressed: () {
-              editing = !editing;
-              setState(() {});
-            },
-            icon: const Icon(Icons.edit),
+    if (contactData!.id != "Loading") {
+      return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: const Text("Details"),
+          leading: BackButton(
+            onPressed: () => removeHighlightOverlay(),
           ),
-        ],
-      ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(14.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              // add circular avatr containing the Thumbnail image of the contact if it has any
-              SizedBox(
-                width: 120,
-                height: 120,
-                child: contactData!.photo != null
-                    ? CircleAvatar(
-                        foregroundImage: MemoryImage(contactData!.photo!),
-                      )
-                    : CircleAvatar(
-                        backgroundColor:
-                            Color(contactData.hashCode).withAlpha(80),
-                        child: Text(
-                          contactData!.displayName[0],
-                          textScaleFactor: 3.5,
+          actions: [
+            IconButton(
+              onPressed: () {
+                editing = !editing;
+                setState(() {});
+              },
+              icon: const Icon(Icons.edit),
+            ),
+          ],
+        ),
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(14.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                // add circular avatr containing the Thumbnail image of the contact if it has any
+                SizedBox(
+                  width: 120,
+                  height: 120,
+                  child: contactData!.photo != null
+                      ? CircleAvatar(
+                          foregroundImage: MemoryImage(contactData!.photo!),
+                        )
+                      : CircleAvatar(
+                          backgroundColor:
+                              Color(contactData.hashCode).withAlpha(80),
+                          child: Text(
+                            contactData!.displayName[0],
+                            textScaleFactor: 3.5,
+                          ),
+                        ),
+                ),
+                Text(
+                  contactData!.displayName,
+                  style: Theme.of(context).textTheme.headlineLarge,
+                ),
+                if (editing)
+                  ListTile(
+                    leading: const Icon(Icons.numbers_sharp),
+                    title: TextField(
+                      keyboardType: TextInputType.number,
+                      maxLines: 1,
+                      onSubmitted: (String value) {
+                        contactData!.phones.add(Phone(value));
+                        FlutterContacts.updateContact(contactData!);
+                        editing = false;
+                        setState(() {});
+                      },
+                    ),
+                  ),
+                for (var numberEntries in contactData!.phones)
+                  ListTile(
+                    title: Text(numberEntries.number),
+                    leading: IconButton(
+                      onPressed: () => launchUrl(
+                        Uri(
+                          scheme: 'tel',
+                          path: numberEntries.number,
                         ),
                       ),
-              ),
-              Text(
-                contactData!.displayName,
-                style: Theme.of(context).textTheme.headlineLarge,
-              ),
-              if (editing)
-                ListTile(
-                  leading: const Icon(Icons.numbers_sharp),
-                  title: TextField(
-                    keyboardType: TextInputType.number,
-                    maxLines: 1,
-                    onSubmitted: (String value) {
-                      contactData!.phones.add(Phone(value));
-                      FlutterContacts.updateContact(contactData!);
-                      editing = false;
-                      setState(() {});
-                    },
-                  ),
-                ),
-              for (var numberEntries in contactData!.phones)
-                ListTile(
-                  title: Text(numberEntries.number),
-                  leading: IconButton(
-                    onPressed: () => launchUrl(
-                      Uri(
-                        scheme: 'tel',
-                        path: numberEntries.number,
-                      ),
+                      icon: const Icon(Icons.call),
                     ),
-                    icon: const Icon(Icons.call),
+                    trailing: IconButton(
+                        onPressed: () => {
+                              contactData!.phones.remove(numberEntries),
+                              FlutterContacts.updateContact(contactData!),
+                              setState(() {}),
+                            },
+                        icon: const Icon(Icons.delete)),
                   ),
-                  trailing: IconButton(
-                      onPressed: () => {
-                            contactData!.phones.remove(numberEntries),
-                            FlutterContacts.updateContact(contactData!),
-                            setState(() {}),
-                          },
-                      icon: const Icon(Icons.delete)),
-                ),
-            ],
+              ],
+            ),
           ),
         ),
-      ),
-    );
+      );
+    } else {
+      return const Scaffold(
+        appBar: MainAppBar(widgetName: 'Loading'),
+      );
+    }
   }
 
   @override
